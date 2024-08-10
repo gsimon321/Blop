b127eab63aa155e56a7e42a15a4d867c4806dfb1
gsimon321 <GS3158@zebra.com>
2024-08-10 15:33:58 -0400

    BLOP-1: Overwritting save data with a tmp file

--- main.go
+++ main.go
@@ -41,7 +41,23 @@ func SaveData1(path string, data []byte) error {
 func SaveData2(path string, data []byte) error {
 	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
 	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
-
+	if err != nil {
+		return err
+	}
+	defer func() {
+		fp.Close()
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
+	if _, err = fp.Write(data); err != nil { // 1. save to the temporary file
+		return err
+	}
+	if err = fp.Sync(); err != nil { //2. fsync
+		return err
+	}
+	err = os.Rename(tmp, path) // 3. replace the temp file with
+	return err
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
