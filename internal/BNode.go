157d9c92f1642c06b55857cf28ca79682588eb85
gsimon321 <GS3158@zebra.com>
2024-08-18 22:10:54 -0400

    BLOP-4: Actual structure added

--- internal/BNode.go
+++ internal/BNode.go
@@ -1,1 +1,12 @@
 package internal
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	panic(node1max <= BTREE_PAGE_SIZE) // maximum KV
+}
