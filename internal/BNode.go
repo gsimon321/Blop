7bed65c825d93ec85a1f0c25f7a307305888533c
gsimon321 <gsimon3@uwo.ca>
2024-09-02 20:28:08 -0400

    BLOP-4: adding in additional data defs and helper methods

--- internal/BNode.go
+++ internal/BNode.go
@@ -1,5 +1,7 @@
 package internal
 
+import "encoding/binary"
+
 const HEADER = 4
 
 const BTREE_PAGE_SIZE = 4096
@@ -9,4 +11,30 @@ const BTREE_MAX_VAL_SIZE = 3000
 func init() {
 	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
 	panic(node1max <= BTREE_PAGE_SIZE) // maximum KV
+}
+
+type BNode []byte // can be dumped to the disk
+
+type BTree struct {
+	// pointer (a nonzero page number)
+	root uint64
+	// callbacks for managing on-disk pages
+	get func(uint64) []byte // dereference a pointer
+	new func([]byte) uint64 // allocate a new page
+	del func(uint64)        // deallocate a page
+}
+
+const (
+	BNODE_NODE = 1
+	BNODE_LEAD = 2
+)
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	
 }
